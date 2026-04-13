ce000a37a5652df0584a5c9e12f97c24675888b5
Diego Emmanuel Rodriguez Ruvalcaba <diegoerr04@gmail.com>
2026-04-12 18:23:36 -0600

    :wrench: Fix URL bug

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,7 @@
-export const HOST = import.meta.env.VITE_HOST;
-export const API_BASE_URL = `http://${HOST}:15800`;
-export const WS_BASE_URL = `ws://${HOST}:15800`;
+export const BASE_URL = import.meta.env.VITE_API_URL;
+export const API_BASE_URL = `http://${BASE_URL}`;
+export const WS_BASE_URL = `ws://${BASE_URL}`;
+
 export const API_KEY = import.meta.env.VITE_API_KEY;
 export const noActionsServices = [
     "mrmikedevs-os-backend.service",
